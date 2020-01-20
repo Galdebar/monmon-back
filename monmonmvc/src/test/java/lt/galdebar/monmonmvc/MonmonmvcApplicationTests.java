29f979caf340d5ed7d02bbf2fb75fe1f7d302d89
Galdebar <Galdebar@gmail.com>
2020-01-20 18:02:24 +0200

    Resetting blank app (because learning).

---
+++ monmonmvc/src/test/java/lt/galdebar/monmonmvc/MonmonmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.galdebar.monmonmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonmonmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
