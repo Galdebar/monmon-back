7343fc944b2811eeb792bd1528751ca5b7809cc1
Galdebar <Galdebar@gmail.com>
2020-08-17 15:47:21 +0300

    adding a log line to better track if the app load event is triggered.

--- src/main/java/lt/galdebar/monmonapi/webscraper/scheduledtasks/RunScraper.java
+++ src/main/java/lt/galdebar/monmonapi/webscraper/scheduledtasks/RunScraper.java
@@ -1,5 +1,6 @@
 package lt.galdebar.monmonapi.webscraper.scheduledtasks;
 
+import lombok.extern.log4j.Log4j2;
 import lt.galdebar.monmonapi.webscraper.services.WebScraperAPI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
@@ -7,6 +8,7 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 @Component
+@Log4j2
 public class RunScraper implements ApplicationListener<ContextRefreshedEvent> {
 
     @Autowired
@@ -15,6 +17,7 @@ public class RunScraper implements ApplicationListener<ContextRefreshedEvent> {
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        log.info("Running on application refresh tasks.");
         webScraperAPI.runScrapers();
     }
 }
